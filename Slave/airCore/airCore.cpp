659e75c06a6b20e09fa14a51b004bb679344283a
Vines <Robotics34955859+vinesmsuic@users.noreply.github.com>
2019-05-27 16:26:42 +0800

    pls dont put zip file

--- Slave/airCore/airCore.cpp
+++ Slave/airCore/airCore.cpp
@@ -1,147 +1,184 @@
-//version 1 23/5/2019
+//version 2.0 23/5/2019
+//v2.0 lock 
 
 #include "airCore.h"
 //              Charge, Release 
 
 
 //construstor
-airCore::airCore(bool debug, PinName pin1, PinName pin2, PinName pin3, PinName pin4){
-    airCoreDebug = debug;
-    rotationalC = new Relay(pin1, pin2);
-    linearC = new Relay(pin3,pin4);
+airCore::airCore(bool debug, PinName pin1, PinName pin2, PinName pin3, PinName pin4) {
+	airCoreDebug = debug;
+	rotationalC = new Relay(pin1, pin2);
+	linearC = new Relay(pin3, pin4);
+	lock = true;
 }
 
 
 //set flag
-void airCore::setAirCoreDebug(bool inp){
-    airCoreDebug = inp; 
-    if (inp){
-        printf("[airCore]debug:ON\n");
-    }else{
-        printf("[airCore]debug:OFF\n");
-    }
+void airCore::setAirCoreDebug(bool inp) {
+	airCoreDebug = inp;
+	if (inp) {
+		printf("[airCore]debug:ON\n");
+	}
+	else {
+		printf("[airCore]debug:OFF\n");
+	}
 }
 
-bool airCore::getAirCoreDebug(void){
-    return airCoreDebug;
+bool airCore::getAirCoreDebug(void) {
+	return airCoreDebug;
 }
 
 
 //basic movements
-void airCore::openC(void){
-    linearC->offRelease();
-    wait(0.1);
-    linearC->onCharge();
-    wait(0.1);
-    
-    if (airCoreDebug){
-        printf("[airCore]OpenC\n");
-    }
+void airCore::openC(void) {
+	linearC->offRelease();
+	wait(0.1);
+	linearC->onCharge();
+	wait(0.1);
+
+	if (airCoreDebug) {
+		printf("[airCore]OpenC\n");
+	}
 }
 
-void airCore::closeC(void){
-    linearC->offCharge();
-    wait(0.1);
-    linearC->onRelease();
-    wait(0.1);
-    
-    if (airCoreDebug){
-        printf("[airCore]CloseC\n");
-    }
+void airCore::closeC(void) {
+	linearC->offCharge();
+	wait(0.1);
+	linearC->onRelease();
+	wait(0.1);
+
+	if (airCoreDebug) {
+		printf("[airCore]CloseC\n");
+	}
 }
 
-void airCore::moveUp(void){
-    rotationalC->offRelease();
-    wait(0.1);
-    rotationalC->onCharge();
-    wait(0.1);
-    
-    if (airCoreDebug){
-        printf("[airCore]Move up\n");
-    }
+void airCore::moveUp(void) {
+	rotationalC->offRelease();
+	wait(0.1);
+	rotationalC->onCharge();
+	wait(0.1);
+
+	if (airCoreDebug) {
+		printf("[airCore]Move up\n");
+	}
 }
 
-void airCore::moveDown(void){
-    rotationalC->offCharge();
-    wait(0.1);
-    rotationalC->onRelease();
-    wait(0.1);
-    
-    if (airCoreDebug){
-        printf("[airCore]Move down\n");
-    }
+void airCore::moveDown(void) {
+	rotationalC->offCharge();
+	wait(0.1);
+	rotationalC->onRelease();
+	wait(0.1);
+
+	if (airCoreDebug) {
+		printf("[airCore]Move down\n");
+	}
 }
 
 
 //pro. s** movements
-void airCore::init(void){
-    if (airCoreDebug){
-        printf("[airCore]init: start\n");
-    }
-    
-    moveDown();
-    closeC();
-    
-    if (airCoreDebug){
-        printf("[airCore]init: end\n");
-    }
+void airCore::init(void) {
+	if (airCoreDebug) {
+		printf("[airCore]init: start\n");
+	}
+
+	moveDown();
+	closeC();
+
+	if (airCoreDebug) {
+		printf("[airCore]init: end\n");
+	}
+
+	lock = false;
 }
 
-void airCore::fullSequence(float time){
-    
-    if (airCoreDebug){
-        printf("[airCore]fullSequence: start\n");
-    }
-        
-    ////////////////////////
-    //hold s*** and move up
-    openC(); // init
-    wait(time/2);
-    
-    
-    ////////////////////////
-    closeC(); // hold the shit
-    wait(time*2);
-    moveUp(); //rise the shit
-    wait(time);
-        
-    //open and close
-    openC(); 
-    wait(time/2);
-    closeC();
-    wait(time/2);
-    
-    /////////////////////////
-    openC();
-    wait(time*2);
-    //back to ready
-    moveDown();
-    wait(time);
-    closeC(); //back to init
-    
-    if (airCoreDebug){
-        printf("[airCore]fullSequence: end\n");
-    }
+void airCore::fullSequence(float time) {
+	if (lock) {
+
+		printf("[airCore]ERROR:some else is running")
+
+	}
+	else {
+		lock = true;
+
+		if (airCoreDebug) {
+			printf("[airCore]fullSequence: start\n");
+		}
+
+		////////////////////////
+		//hold s*** and move up
+		openC(); // init
+		wait(time / 2);
+
+
+		////////////////////////
+		closeC(); // hold the shit
+		wait(time * 2);
+		moveUp(); //rise the shit
+		wait(time);
+
+		//open and close
+		openC();
+		wait(time / 2);
+		closeC();
+		wait(time / 2);
+
+		/////////////////////////
+		openC();
+		wait(time * 2);
+		//back to ready
+		moveDown();
+		wait(time);
+		closeC(); //back to init
+
+		if (airCoreDebug) {
+			printf("[airCore]fullSequence: end\n");
+		}
+
+		lock = false;
+
+	}
 }
 
-void airCore::hold(float time){
-    
-    closeC(); // hold the shit
-    wait(time*2);
-    moveUp(); //rise the shit
-    wait(time);
+void airCore::hold(float time) {
+	if (lock) {
 
-    openC(); 
-    wait(time/2);
-    closeC();
-    wait(time/2);
+		printf("[airCore]ERROR:some else is running")
+
+	}
+	else {
+		lock = true;
+
+		closeC(); // hold the shit
+		wait(time * 2);
+		moveUp(); //rise the shit
+		wait(time);
+
+		openC();
+		wait(time / 2);
+		closeC();
+		wait(time / 2);
+
+		lock = false;
+	}
 }
 
-void airCore::aim(float time){
-        
-    openC();
-    wait(time*2);
-    moveDown(); //back to ready
-    wait(time);
-    closeC(); //back to init
+void airCore::aim(float time) {
+
+	if (lock) {
+
+		printf("[airCore]ERROR:some else is running")
+
+	}
+	else {
+		lock = true;
+
+		openC();
+		wait(time * 2);
+		moveDown(); //back to ready
+		wait(time);
+		closeC(); //back to init
+
+		lock = false;
+	}
 }
--- Slave/airCore/airCore.h
+++ Slave/airCore/airCore.h
@@ -2,27 +2,27 @@
 #include "Relay.h"
 
 class airCore
-{ 
+{
 public:
-    airCore(bool debug, PinName pin1, PinName pin2, PinName pin3, PinName pin4);
+	airCore(bool debug, PinName pin1, PinName pin2, PinName pin3, PinName pin4);
 
-    void setAirCoreDebug(bool inp);
-    bool getAirCoreDebug(void);
-    
-    void openC(void);
-    void closeC(void);
-    void moveUp(void);
-    void moveDown(void);
-    
-    void init(void);
-    
-    void fullSequence(float time);
-    void hold(float time);
-    void aim(float time);
-    
-private: 
-    bool airCoreDebug;
-    
-    Relay *rotationalC;
-    Relay *linearC;
+	void setAirCoreDebug(bool inp);
+	bool getAirCoreDebug(void);
+
+	void openC(void);
+	void closeC(void);
+	void moveUp(void);
+	void moveDown(void);
+
+	void init(void);
+
+	void fullSequence(float time);
+	void hold(float time);
+	void aim(float time);
+
+private:
+	bool airCoreDebug;
+	bool lock;
+	Relay *rotationalC;
+	Relay *linearC;
 };
